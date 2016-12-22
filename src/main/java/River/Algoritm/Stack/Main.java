0fa14d054aaf6c9e7c494a70a188bfc4e677d1da
River <3mjane@gmail.com>
2016-12-22 22:44:08 +0300

    stack delete equals first

--- src/main/java/River/Algoritm/Stack/Main.java
+++ src/main/java/River/Algoritm/Stack/Main.java
@@ -1,9 +1,44 @@
 package River.Algoritm.Stack;
 
+import River.Algoritm.Stack.Exceptions.EmptyStackException;
+import River.Algoritm.Stack.Exceptions.StackOverflowExeption;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws StackOverflowExeption, EmptyStackException {
+        Stack sArray = new StackArray(20);
+        sArray.push(7);
+        sArray.push(6);
+        sArray.push(1);
+        sArray.push(5);
+        sArray.push(4);
+        sArray.push(1);
+        sArray.push(3);
+        sArray.push(2);
+        sArray.push(1);
+
+        System.out.println(sArray.toString());
+
+        deleteEqualsFirst(sArray);
+
+        System.out.println(sArray.toString());
 
     }
 
-
+    static void deleteEqualsFirst(Stack stack) throws EmptyStackException, StackOverflowExeption {
+        Stack temp = new StackList();
+        int first;
+        int current;
+        if (!stack.isEmpty()) {
+            first = stack.pop();
+            while (!stack.isEmpty()) {
+                current = stack.pop();
+                if (current != first) {
+                    temp.push(current);
+                }
+            }
+            while (!temp.isEmpty()) {
+                stack.push(temp.pop());
+            }
+        }
+    }
 }
